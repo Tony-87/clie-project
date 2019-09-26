478c17b7d13561be6b39b04c43abecf8676e11d3
BobbyWang <xiaoboit@163.com>
2019-09-26 08:58:38 +0800

    mescroll plugin

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import mhome from '@/pages/home'
+import listNews from '@/pages/base/list-news'
+import mescrollOptions from '@/pages/base/mescroll-options'
+import listProducts from '@/pages/base/list-products'
+import mescrollComponent from '@/pages/base/mescroll-component'
+import mescrollMore from '@/pages/base/mescroll-more'
+import mescrollSwiperNav from '@/pages/base/mescroll-swiper-nav'
 
 Vue.use(Router)
 
@@ -18,6 +25,35 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/mhome',
+      name: 'mhome',
+      component: mhome
+    }, {
+      path: '/listNews',
+      name: 'listNews',
+      component: listNews
+    }, {
+      path: '/mescrollOptions',
+      name: 'mescrollOptions',
+      component: mescrollOptions
+    }, {
+      path: '/listProducts',
+      name: 'listProducts',
+      component: listProducts
+    }, {
+      path: '/mescrollComponent',
+      name: 'mescrollComponent',
+      component: mescrollComponent
+    }, {
+      path: '/mescrollMore',
+      name: 'mescrollMore',
+      component: mescrollMore
+    }, {
+      path: '/mescrollSwiperNav',
+      name: 'mescrollSwiperNav',
+      component: mescrollSwiperNav
     }
   ]
 })
