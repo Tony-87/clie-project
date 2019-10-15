1b8870c31a19ea41c86d164d8ed0307f6602d7d5
wangxiaobo9 <wangxiaobo9@jd.com>
2019-10-15 10:54:54 +0800

    popEvent

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <hr />
+
+    <div class="outer-box" @click="outerClick">
+      <div class="inner-box" @click.stop="innerClick">
+        <button @click="btnClick">button</button>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    outerClick () {
+      console.log('outter Click')
+    },
+    innerClick () {
+      console.log('inner Click')
+    },
+    btnClick () {
+      console.log('btn Click')
+    }
+  }
+}
+</script>
+<style lang="less">
+.outer-box {
+  border: solid 1px #f00;
+  padding: 20px;
+}
+.inner-box {
+  border: solid 1px #00f;
+  padding: 20px;
+}
+</style>
