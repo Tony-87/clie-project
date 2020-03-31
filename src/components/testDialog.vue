b188855565397bd0289e6b858028ca921791768e
wangxiaobo33 <wangxiaobo9@jd.com>
2020-03-31 16:13:00 +0800

    sync demo

---
+++ src/components/testDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+    <span>这是一段信息</span>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: ['testVisible'],
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    dialogVisible: {
+      get() {
+        console.log(this.testVisible);
+
+        return this.testVisible
+      },
+      set(val) {
+        console.log('set val=', val);
+
+        this.$emit('update:testVisible', val)
+      }
+    }
+  },
+  methods: {
+    handleClose() {
+      this.dialogVisible = false
+    }
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <el-button @click="openDialog">打开弹窗</el-button>
+
+    <testDialog :test-visible.sync="testDialogVisible"></testDialog>
   </div>
 </template>
+<script>
+import testDialog from '../components/testDialog'
+export default {
+  components: {
+    testDialog
+  },
+  data() {
+    return {
+      testDialogVisible: false
+    }
+  },
+  methods: {
+    openDialog() {
+      this.testDialogVisible = true
+    }
+  }
+}
+</script>
