dcfb9cbbcfed7add1439187d0253b67070d2ea3f
wangxiaobo33 <wangxiaobo9@jd.com>
2022-07-11 15:34:39 +0800

    feat:陀螺仪

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,57 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-
-    <hr />
-    <button @click="askDevicePermission">get imu</button>
-    <br />
-    <button @touchstart="askDevicePermission">touchstart get imu</button>
-    <br />
-    <button @touchmove="askDevicePermission">touchmove get imu</button>
-    <br />
-    <button @touchend="askDevicePermission">touchend get imu</button>
-    <br />
-    <h2>imu data</h2>
-    <div>alpha: {{imuData.alpha}}</div>
-    <div>beta: {{imuData.beta}}</div>
-    <div>gamma: {{imuData.gamma}}</div>
+    <iframe  src="https://predfq.jd.com/pse/edit.html#?materialId=59239" width="800" height="600" ></iframe>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
+  data () {
     return {
       list: [1, 2, 3],
       imuData: {}
     }
   },
-  mounted() {
-    this.initIMUEvent()
+  mounted () {
+    // https://pscpre.jd.com/psc/getDesignToolMaterial?sceneRenderId=59239
+    fetch('/api/psc/getDesignToolMaterial?sceneRenderId=59239')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+      })
   },
   methods: {
-    askDevicePermission() {
-      if (window.DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === 'function') {
-        DeviceMotionEvent.requestPermission()
-          .then(response => {
-            if (response == 'granted') {
-              window.addEventListener('devicemotion', (e) => {
 
-              })
-            }
-          })
-          .catch(console.error)
-
-      }
-    },
-    initIMUEvent() {
-      console.log('window.DeviceMotionEvent', window.DeviceMotionEvent);
-
-      window.addEventListener('deviceorientation', (e) => {
-        console.log(e)
-        this.imuData = e
-      })
-    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,28 @@
 
 module.exports = {
-    publicPath: './',
-    filenameHashing: true,
-    configureWebpack: {
-        devServer: {
-            disableHostCheck: true,
+  publicPath: './',
+  filenameHashing: true,
+  configureWebpack: {
+    devServer: {
+      disableHostCheck: true
 
+    }
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].minify = false
+      return args
+    })
+  },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://pscpre.jd.com/',
+        pathRewrite: {
+          '^/api': ''
         }
-    },
-    chainWebpack: config => {
-        config.plugin('html').tap(args => {
-            args[0].minify = false
-            return args
-        })
-    },
+      }
+    }
+  }
 
 }
